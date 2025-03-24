9bcfb2a1dceab5ef256755c0f43d0c35ebc78876
jacob moya <jemoya1994@Gmail.com>
2025-03-23 20:50:57 -0700

    started optimizing the theme a little bit more - taking a React Break Until my next project, some refactoring updates possibly - always a marathon!

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+    <title>Document</title>
+</head>
+
+<body <?php body_class() ?> >
+
+<header>    
+    <nav>
+        <ul>
+            <li>Item1</li>
+            <li>Item1</li>
+            <li>Item1</li>
+        </ul>
+    </nav>
+</header>
+
